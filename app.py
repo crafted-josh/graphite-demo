0518ae14e3451c20124d0424fa881eadffabdc70
Josh Ladd <josh@crafted.solutions>
2023-11-10 08:40:04 -0600

    MYBLDR-2708:Add initial page and feature flag

--- app.py
+++ app.py
@@ -2,6 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+feature_flags = {
+    "new_page_flag": True
+}
+
+if feature_flags.get("new_page_flag"):
+    @app.route('/new_page')
+    def newpage():
+        return render_template('new_page.html')
+
 @app.route('/')
 def index():
     return render_template('index.html')
--- test_app.py
+++ test_app.py
@@ -1,7 +1,18 @@
-from app import app
+from app import app, feature_flags
 
 def test_index():
     with app.test_client() as client:
         response = client.get('/')
         assert response.status_code == 200
         assert b"Welcome to the Flask App!" in response.data
+
+def test_newpage_with_feature_flag():
+    if feature_flags.get("new_page_flag"):
+        with app.test_client() as client:
+            response = client.get('/newpage')
+            assert response.status_code == 200
+            assert b"This is the New Page!" in response.data
+    else:
+        with app.test_client() as client:
+            response = client.get('/newpage')
+            assert response.status_code == 404
